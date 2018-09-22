fdd5bd0c8cbad385641d8c9f3ccf728c89ea6147
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-22 19:43:55 -0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import os
+
+bot = commands.Bot(command_prefix='yato ')
+ownerID = "274298631517896704"
+
+# To remove the help command and make your own help command
+#bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+  print ("------")
+  print ("My name is " + bot.user.name)
+  print ("With the ID: " + bot.user.id)
+  print ("Using discord.py v" + discord.__version__)
+  print ("------")

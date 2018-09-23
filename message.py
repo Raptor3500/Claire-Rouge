4d2c3ec0731ebb65a91f59ca69636d379c3d4281
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-23 02:17:43 -0400

    Update message.py

--- message.py
+++ message.py
@@ -7,10 +7,6 @@ class message():
     async def on_message(self, message):
         if 'pervert' in message.content:
             await self.bot.send_message(message.channel, "I'll burn him to cinders")
-            
-    async def on_message(self, message):
-        if '┬─┬ ノ( ゜-゜ノ)' in message.content:
-            await self.bot.send_message(message.channel, "(╯°□°）╯︵ ┻━┻")
 
 def setup(bot):
     bot.add_cog(message(bot))

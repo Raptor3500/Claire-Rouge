9935a7a551a60cb5c65938d32109cff654ff22c4
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-23 01:39:28 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def say(ctx, *args):
     """Make me say your message"""
+    if ctx.message.author.id in ownerID:
         channel = ctx.message.channel
         mesg = ' '.join(args)
         await bot.delete_message(ctx.message)
@@ -30,6 +31,31 @@ async def say(ctx, *args):
         await asyncio.sleep(1)
         await bot.say(mesg)
         print (ctx.message.author.id + " or " + ctx.message.author.name + " made me say '{}'".format(mesg))
+
+startup_extensions = [
+  message
+]
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+            
+import discord
+
+class message():
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        if 'I want to die' in message.content:
+            await self.bot.send_message(message.channel, 'Then kill yourself already.')
+
+def setup(bot):
+    bot.add_cog(message(bot))
          
   
 

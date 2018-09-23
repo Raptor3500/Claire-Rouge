b3c3a452584708333e2554ab38b4fbecfa8f4224
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-22 20:01:58 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import os
 
 bot = commands.Bot(command_prefix='yato ')
 ownerID = "274298631517896704"
+ownerID2 = "329337654850093056"
 
 # To remove the help command and make your own help command
 #bot.remove_command('help')
@@ -18,4 +19,67 @@ async def on_ready():
   print ("Using discord.py v" + discord.__version__)
   print ("------")
   
-  bot.run(os.environ.get('Token'))
+  # Make me say stuff
+@bot.command(pass_context=True)
+async def say(ctx, *args):
+    """Make me say your message"""
+    if ctx.message.author.id in ownerID:
+        channel = ctx.message.channel
+        mesg = ' '.join(args)
+        await bot.delete_message(ctx.message)
+        await bot.send_typing(channel)
+        await asyncio.sleep(1)
+        await bot.say(mesg)
+        print (ctx.message.author.id + " or " + ctx.message.author.name + " made me say '{}'".format(mesg))
+    else:
+      channel = ctx.message.channel
+      mesg = ' '.join(args)
+      await bot.delete_message(ctx.message)
+      await bot.send_typing(channel)
+      await asyncio.sleep(1)
+      await bot.say(mesg)
+      print (ctx.message.author.id + " or " + ctx.message.author.name + " made me say '{}'".format(mesg))
+      
+      
+@bot.command(pass_context=True)
+async def kill(ctx, user: discord.Member=None):
+        if user is None:
+            await bot.say(ctx.message.author.mention + ": I can't kill anyone unless you tell me who to kill!")
+            return
+        if user.id == "489775956731363328":
+            await bot.say(ctx.message.author.mention + ": I won't let you kill me! :knife:")
+        elif user.id == ownerID:
+            await bot.say(ctx.message.author.mention + ": Why do you want to kill my developer?")
+        elif user.id == ctx.message.author.id:
+            await bot.say(ctx.message.author.mention + ": Why do you want me to kill you?")
+        else:
+            await bot.say("I have killed {}".format(user.name))
+        if user.id == "400418638240415745":
+          await bot.say("God vs a God huh Sekki!! Sorry Im not losing this one")
+            
+#invite me
+@bot.command(pass_context=True)
+async def invite(ctx):
+  """Invite me"""
+  await bot.say("God Yato speaking you want me in your server?")
+  await bot.say("https://discordapp.com/api/oauth2/authorize?client_id=489775956731363328&permissions=8&scope=bot")
+  await bot.say("here you go that'll be five yen")
+  
+@bot.command(pass_context=True)
+async def insult(ctx, user: discord.Member=None):
+  """Make me insult someone"""
+  if user is None:
+    await bot.say("I cant insult air just because im a god doesnt mean I can do everything")
+    return
+  if user.id == ownerID:
+    await bot.say("My developer is so useless he cant even make a bot on his own just look at Rem, Agent did most of the work on her.")
+  if user.id == "329337654850093056":
+    await bot.say("You stupid cuck You are Big Gae just ask Trunks")
+  if user.id == "462099439784427523":
+    await bot.say("You say you're a good person but yet you intentionally piss people off and make people more depressed")
+         
+  
+
+
+
+bot.run(os.environ.get('Token'))

435fe773891a62fc80884396db9318f14ee9c980
Raptor3500 <37379722+Raptor3500@users.noreply.github.com>
2018-09-23 01:49:45 -0400

    Create message.py

---
+++ message.py
@@ -0,0 +1,24 @@
+startup_extensions = [
+  'message'
+]
+
+if __name__ == "__main__":
+    for extension in startup_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            exc = '{}: {}'.format(type(e).__name__, e)
+            print('Failed to load extension {}\n{}'.format(extension, exc))
+            
+import discord
+
+class message():
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def on_message(self, message):
+        if 'I want to die' in message.content:
+            await self.bot.send_message(message.channel, 'Then kill yourself already.')
+
+def setup(bot):
+    bot.add_cog(message(bot))

d2f39b37237be6672c11544f8ba7241c8f394c80
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-01 09:27:44 -0400

    Update message.py

--- message.py
+++ message.py
@@ -1,8 +1,5 @@
 import discord
 
-user = 'rZuTJIFKDZF5oi0T'
-key = 'jiaN5JDdXrvjRNFng4'
-
 class message():
     def __init__(self, bot):
         self.bot = bot
@@ -10,14 +7,6 @@ class message():
     async def on_message(self, message):
         if 'pervert' in message.content:
             await self.bot.send_message(message.channel, "I'll burn him to cinders")
-            
-    async def on_message(message):
-        if not message.author.bot and (message.server == None or client.user in message.mentions):
-            await client.send_typing(message.channel)
-            txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
-        r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'frost', 'text':txt}).text)
-        if r['status'] == 'success':
-            await client.send_message(message.channel, r['response'] )
 
 
 

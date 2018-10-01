120f6ba841cc0098e9274662cf45a5e7f440705c
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-01 09:46:57 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ async def on_ready():
   
 @bot.event
 async def on_message(message):
-  if not message.author.bot and (message.server == None or client.user in message.mentions):
+  if not message.author.bot and (message.server == None or bot.user in message.mentions):
     await client.send_typing(message.channel)
     txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
     r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'claire', 'text':txt}).text)

d2d1b5bb27e278c84ccb9ec2149397bd368af04b
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-01 16:53:16 -0400

    Update message.py

--- message.py
+++ message.py
@@ -18,7 +18,7 @@ class message():
             await self.bot.send_typing(message.channel)
             txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
             r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'claire', 'text':txt}).text)
-            await bot.send_message(message.channel, r['response'] )
+            await self.bot.send_message(message.channel, r['response'] )
 
 
 
